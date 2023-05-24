04d756072458fbee4ba41d7dd4059aee52ffbcf2
ArturPiskorski <134394853+ArturPiskorski@users.noreply.github.com>
2023-05-24 19:05:46 +0200

    Update texttosha1.html

--- texttosha1.js
+++ texttosha1.js
@@ -1,9 +1,14 @@
+<html>
+  <head>
+    <script src="https://thunkable.github.io/webviewer-extension/thunkableWebviewerExtension.js" type="text/javascript"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jsSHA/2.4.0/sha1.js"></script>
+ </head>
+ <body>
 <script>
   function receivePostData(event) {
     var data = event.data;
     var hashedString = sha1(data);
-    window.parent.postMessage(hashedString, "*");
+    ThunkableWebviewerExtension.postMessage(hashedString, "*");
   }
 
   function sha1(message) {
@@ -11,6 +16,7 @@
     shaObj.update(message);
     return shaObj.getHash("HEX");
   }
-
   window.addEventListener("message", receivePostData);
 </script>
+ </body>
+</html>
